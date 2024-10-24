98c3b75d3a45dbe7e58e9c618ba64ae226d76c15
Sowrasree <sourashree03@gmail.com>
2024-10-25 00:19:55 +0530

    more changes to locations sections

--- src/Components/Locations.js
+++ src/Components/Locations.js
@@ -7,6 +7,25 @@ const Locations = ()=>{
             giving you the space to live and the service to clean up after. Reception, gym and food goodies? 
             We got those too.
             </p>
+            <div className="text-left h-60 w-60">
+                <ul className="text-sm font-light">
+                    <li className="pt-12">
+                        ALL LOCATIONS
+                    </li>
+                    <li className="pt-4">
+                        AMSTERDAM
+                    </li>
+                    <li className="pt-4">
+                        LONDON
+                    </li>
+                    <li className="pt-4">
+                        DUBLIN
+                    </li>
+                    <li className="pt-4">
+                        LISBON
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
